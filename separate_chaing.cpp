666faf14468d6194a612c5427f8034361807758c
Umair Farooq <mirumair7893@gmail.com>
2026-05-11 01:10:02 +0530

    added code

--- separate_chaing.cpp
+++ separate_chaing.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class HashTableSC {
-private:
-    int size;
-    vector<vector<int>> table;
-    int hash(int key) {
-        return key % size;
-    }
-public:
-    HashTableSC(int s) {
-        size = s;
-        table.resize(size);
-    }
 
-    void insert(int key) {
-        int index = hash(key);
-        table[index].push_back(key);
-    }
+// Hash function: gives index using modulo
+int hashFunc(int key, int size) {
+    return key % size;   // %10 used in this case
+}
 
-    void display() {
-        for (int i = 0; i < size; i++) {
-            cout << "Index " << i << ": ";
-            for (int val : table[i]) {
-                cout << val << " ";
-            }
-            cout << endl;
-        }
-    }
-};
 int main() {
-    HashTableSC hashTable(10);
+    int size = 10;  // size of hash table
+
+    // 2D vector for separate chaining
+    // Each index stores a list (chain) of values
+    vector<vector<int>> table(size);
+
+    // Input keys
     int keys[] = {15, 25, 35, 45};
     int n = sizeof(keys) / sizeof(keys[0]);
+
+    // ---------------- INSERTION ----------------
     for (int i = 0; i < n; i++) {
-        hashTable.insert(keys[i]);
+        int index = hashFunc(keys[i], size);  
+        // Find index using hash function
+
+        table[index].push_back(keys[i]);      
+        // Insert key into that index's chain
     }
-    hashTable.display();
+
+    // ---------------- DISPLAY ----------------
+    for (int i = 0; i < size; i++) {
+        cout << "Index " << i << ": ";
+
+        // Print all elements in chain at index i
+        for (int val : table[i]) {
+            cout << val << " ";
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
 
